12982a711083c04b71a266ceb83acb7eef6a4778
Marcel de Groot <mg.degroot@avans.nl>
2023-08-31 10:54:41 +0200

    added examples, removed syntax error

--- CSharpConsoleDemo/Program.cs
+++ CSharpConsoleDemo/Program.cs
@@ -1,33 +1,59 @@
 ﻿// Global using statements, geen dubbele using statements nodig in de code
 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-10.0/global-usings
 // Zet hier ALLEEN using statements die je in VEEL code files nodig hebt
+// (conventie is deze in apart cs bestand genaamd 'globalusings.cs' te zetten) -->
 global using System;
 global using System.Linq;
 using CSharpConsoleDemo;
+using System.IO.Pipes;
 
 
 // Top level statements, geen Main() nodig
-// Wel beperkt tot 1 bestand per project (compilation-unit / assembly)
+// Wel beperkt tot 1 bestand per project (compilation-unit / assembly) -->
 // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/csharp-9.0/top-level-statements
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("The answer to the question of Life, The Universe and Everything?");
+var answer = Console.ReadLine();
+if (!int.TryParse(answer, out int answerAsInt))
+{
+    Console.WriteLine("It is a number...");
+}
+else if( answerAsInt == 42)
+{
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Wrong!");
+}
+
+// Pattern matching -->
+var result = answer switch
+{
+    string s when s.Length > 2 => "String length is greater then 2, Wrong...", // <-- 
+    string s when int.TryParse(s, out int i) && i == 42 => "Correct!", // <-- probeer 'anwer te parsen als int en controleer waarde
+    "42" => "Correct!", // <-- match als string (zal niet geraakt worden want vorige case matcht ook)
+    _ => "Wrong!", // <-- default met discard pattern
+};
+
+
 
 // Explicit type met object initializer syntax -->
 
-Student explicitStudent = new Student { Nr = 1, FirstName = "Jan", LastName = "Janssens", DateOfBirth = new DateOnly(2000, 1, 1) };
+Student explicitStudent = new Student { Nr = 1, FirstName = "Twoflower", LastName = "The Tourist", DateOfBirth = new DateOnly(2000, 1, 1) };
 
 // Implicit type -->
 var implicitStudent = new Student();
 implicitStudent.Nr = 2;
-implicitStudent.FirstName = "Piet";
-implicitStudent.LastName = "Pieters";
+implicitStudent.FirstName = "Ponder";
+implicitStudent.LastName = "Stibbons";
 //implicitStudent.DateOfBirth = new DateOnly(2000, 1, 1); <-- kan niet, want init-only property
 
 // Collection initializer -->
 var students = new List<Student>() { 
-    new Student { Nr = 1, FirstName = "Jan", LastName = "Janssens", DateOfBirth = new DateOnly(2000, 1, 1), }, 
-    new Student { Nr = 2, FirstName = "Piet", LastName = "Pieters", },
-    new Student { Nr = 3, FirstName = "Joris", LastName = "Jorissen", /*SlbTeacher = null <-- geen setter, kan alleen in constructor gezet worden */},
+    new Student { Nr = 1, FirstName = "Cohen", LastName = "The Barbarian", DateOfBirth = new DateOnly(2000, 1, 1), }, 
+    new Student { Nr = 2, FirstName = "Cut-Me-Own-Throat", LastName = "Dibbler", },
+    new Student { Nr = 3, FirstName = "Julliet", LastName = "Stollop", /*SlbTeacher = null <-- geen setter, kan alleen in constructor gezet worden */},
 };
 
 
@@ -40,5 +66,39 @@ students.ForEach(s => Console.WriteLine($"{s.Nr} {s.FirstName} {s.LastName}"));
 // zelfde met foreach -->
 foreach (var student in students)
 {
-    Console.WriteLine($"{s.Nr} {s.FirstName} {s.LastName}"));
+    Console.WriteLine($"{student.Nr} {student.FirstName} {student.LastName}");
 }
+
+// LINQ -->
+
+// query syntax -->
+var studentsWithJ_QS =
+    from student in students
+    where student.FirstName.StartsWith("J")
+    select student;
+
+// method syntax -->
+var studentsWithS_MS = students.Where(s => s.FirstName.Contains("S"));
+
+
+// operaties -->
+var eenUnion = studentsWithJ_QS.Union(studentsWithS_MS);
+
+var eenTweedeCollectie = new List<Student>()
+{
+    new Student { Nr = 4, FirstName = "Mustrum", LastName = "Ridcully", },
+    new Student { Nr = 5, FirstName = "Rincewind", LastName = "The Wizard", },
+    new Student { Nr = 6, FirstName = "Bloody Stupid", LastName = "Johnson", },
+};
+
+// transformaties mbv select -->
+// fluent syntax, method chaining -->
+eenTweedeCollectie
+    .Where(s => s.LastName.Contains("i", StringComparison.OrdinalIgnoreCase)) // <-- filter, case insensitive
+    .Select(s => new { s.Nr, s.FirstName, s.LastName }) // <-- transformatie naar anoniem type
+    .ToList() // <-- materialize view (ie: vul geheugen)
+    .ForEach(s => Console.WriteLine($"{s.Nr} {s.FirstName} {s.LastName}")); // <-- itereren over collectie
+
+
+// File IO
+File.WriteAllLines("students.txt", students.Select(s => $"{s.Nr}\t{s.FirstName}\t{s.LastName}\r\n"));
