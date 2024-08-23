275ec5cfac20433bcb38691db9bf9320ae0228c9
mg-degroot <112110393+mg-degroot@users.noreply.github.com>
2024-08-23 12:14:33 +0200

    Upgraded to dotnet 8 and added some nullability examples

--- CSharpConsoleDemo/Student.cs
+++ CSharpConsoleDemo/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,25 +13,35 @@ public class Student : IStudent
 
     public Student()
     {
+        
         SlbTeacher = null;
     }
+    
+    // annotatie (attribuut) geeft aan dat 'required' members hier gezet worden -->
+    [SetsRequiredMembers]
     public Student(Teacher? slbTeacher = null, int nr = 0, string firstName = "")
     {
         SlbTeacher = slbTeacher;
         Nr = nr;
         this.FirstName = firstName;
 
-        Student student = new Student();
+        // MOET FirstName zetten wegens 'required' -->
+        Student student = new Student() { FirstName = firstName };
         student.Nr = 1;
     }
 
     public int Nr { get; set; }
 
-    // Reference type, mag niet null zijn -->
-    public string FirstName { get; set; } = null!;
+    // Reference type, mag eigenlijk niet null zijn -->
+    // Juiste manier: required modifier
+    public required string FirstName { get; set; }
 
     // Nullable reference type, mag wel null zijn -->
-    public string? LastName { get; set; } = null; // = null is niet nodig, default zijn ref types null.
+    public string? Infix { get; set; } = null; // = null is niet nodig, default zijn ref types null.
+
+    // Reference type, mag eigenlijk niet null zijn -->
+    // Niet gebruiken, lelijk: override warning met !
+    public string LastName { get; set; } = null!;
 
     // init-only property, kan alleen in constructor EN via object initialization gezet worden -->
     public DateOnly DateOfBirth { get; init; }
@@ -38,10 +49,10 @@ public class Student : IStudent
     // read-only property, kan alleen in constructor gezet worden -->
     public Teacher? SlbTeacher { get; }
 
-
     // Een full-property met backing field -->
     public bool IsEnlisted { get => _isEnlisted; set => _isEnlisted = value; }
 
+    // Expression-bodied method -->
     public int Test() => 1;
     
 }
--- CSharpConsoleDemo/StudentExensions.cs
+++ CSharpConsoleDemo/StudentExensions.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿// Onnodige imports. Kan geen kwaad maar geeft ruis -->
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CSharpConsoleDemo;
+
+// Static class met extension method -->
 public static class StudentExensions
 {
-    public static string GetFullName(this Student student) => $"{student.FirstName} {student.LastName}";
+    /// <summary>
+    /// Extension method op Student
+    /// (let op de 'this' en static)
+    /// Expression bodied method ('=>')
+    /// null-coalesce operator (??) 
+    /// </summary>
+    /// <param name="student"></param>
+    /// <returns></returns>
+    public static string GetFullName(this Student student) => $"{student.FirstName} {student.Infix ?? ""} {student.LastName}";
+
+    /// <summary>
+    /// Nog een extension method, deze accepteerd een optionele extra parameter
+    /// </summary>
+    /// <param name="student"></param>
+    /// <param name="prefix"></param>
+    /// <returns></returns>
+    public static string AddPrefix(this Student student, string prefix = "") => 
+        $"{prefix} {student.FirstName} {student.Infix ?? ""} {student.LastName}";
 
 }
